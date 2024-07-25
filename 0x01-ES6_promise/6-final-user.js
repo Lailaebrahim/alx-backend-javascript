9e31d2e81939fa0a910532289266d10641850781
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-25 23:38:59 +0300

    fix lint errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ];
 
   const results = await Promise.allSettled(promises);
-  
-  return results.map(result => ({
+
+  return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : String(result.reason)
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
   }));
 }
