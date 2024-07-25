1dfbf94bdba9040995a95878acc653f883dc79a1
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-25 23:59:45 +0300

    add explaination

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
-
+ /*
+ * 1- async before the function declaration allows the use of await inside the function
+  and ensures the function returns a Promise.
+ * 2- await before Promise.allSettled(promises):
+    Pauses the function execution until Promise.allSettled() completes.
+    When Promise.allSettled() resolves, await unwraps its resolved value and assigns it to results.
+    Without await, results would be a Promise object instead of the actual array of results.
+    Both signUpUser() and uploadPhoto() start executing immediately and run concurrently.
+    Promise.allSettled() waits for both promises to settle, regardless of order. */
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
@@ -11,6 +19,8 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
 
   return results.map((result) => ({
     status: result.status,
+    /*first result is the return of the first promise which is the object passed to resolve and can be a string
+    * second result is the result of the second promise which is the error object passed to reject so converts the rejection reason to a string */
     value: result.status === 'fulfilled' ? result.value : String(result.reason),
   }));
 }
