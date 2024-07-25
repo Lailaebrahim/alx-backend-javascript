35bea5dbe701c572a5fa9f65d8130a77c66b9e4f
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-25 23:34:44 +0300

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,5 +8,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   ];
 
   const results = await Promise.allSettled(promises);
-  return results;
+  
+  return results.map(result => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason)
+  }));
 }
