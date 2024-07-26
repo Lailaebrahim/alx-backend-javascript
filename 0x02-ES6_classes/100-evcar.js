80c8f0ff59d1f4d58df418a0e56fd06908915238
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-26 17:37:04 +0300

    Advanced Task

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+export default class EVCar  extends Car{
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Constructor = Object.getPrototypeOf(this).constructor[Symbol.species] || this.constructor;
+    return new Constructor();
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
