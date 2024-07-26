3ba8d556b350f2f9da3548eb4d118ac4c0cd9b76
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-26 16:25:33 +0300

    fix lint errors in Task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,9 +1,9 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== 'number'){
+    if (typeof size !== 'number') {
       throw new TypeError('size must be a number');
     }
-    if (typeof location !== 'string'){
+    if (typeof location !== 'string') {
       throw new TypeError('location must be a string');
     }
     this._size = size;
@@ -15,7 +15,7 @@ export default class HolbertonClass {
   }
 
   set size(size) {
-    if (typeof size !== 'number'){
+    if (typeof size !== 'number') {
       throw new TypeError('size must be a number');
     }
     this._size = size;
@@ -26,13 +26,13 @@ export default class HolbertonClass {
   }
 
   set location(location) {
-    if (typeof location !== 'string'){
+    if (typeof location !== 'string') {
       throw new TypeError('location must be a string');
     }
     this._location = location;
   }
 
-  /*This defines a method using the Symbol.toPrimitive symbol.
+  /* This defines a method using the Symbol.toPrimitive symbol.
    This method will be called when an object needs to be converted to a primitive value. */
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') return this._size;
