fc258a81cfcb2d4913c0cb1b367e3fe591c0a34f
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-30 23:31:05 +0300

    Task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByCity(stds, city, newGrades) {
+  if (!Array.isArray(stds) || !Array.isArray(newGrades)) {
+    return [0];
+  }
+  return stds
+    .filter((std) => std.location === city)
+    .map((std) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === std.id);
+      if (matchingGrade) {
+        std.grade = matchingGrade.grade;
+      }
+      else {
+        std.grade = 'N/A';
+      }
+      return std;
+    });
+}
