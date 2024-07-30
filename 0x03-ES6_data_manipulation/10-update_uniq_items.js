de7889c9ab3315e7496a4a6c5de22ac4eb7e75d3
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-31 01:28:00 +0300

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,12 +2,10 @@ export default function updateUniqueItems(groceriesList) {
   if (!(groceriesList instanceof Map)) {
     throw new Error('Cannot process');
   }
-  
   for (const [key, value] of groceriesList) {
     if (value === 1) {
       groceriesList.set(key, 100);
     }
   }
-  
   return groceriesList;
 }
