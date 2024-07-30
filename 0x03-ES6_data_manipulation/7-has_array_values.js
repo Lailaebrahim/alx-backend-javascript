48f922b594ce328215393ab35768bef41b78c6c6
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-31 00:37:02 +0300

    Task 7

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,14 @@
+export default function hasValuesFromArray(set, array) {
+  let status = false;
+  if (!(set instanceof Set) || !Array.isArray(array)) {
+    return false;
+  }
+  for (let item of array) {
+    if (set.has(item)) {
+      status = true;
+    } else {
+      return false;
+    }
+  }
+  return status;
+}
