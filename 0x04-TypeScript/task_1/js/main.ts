ae24f3e546bd4c229e44adda8d9adb95f9f49e1f
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-31 21:58:31 +0300

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,33 @@ interface printTeacherFunction {
 export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  
+  constructor (firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+const student: StudentConstructor = StudentClass;
