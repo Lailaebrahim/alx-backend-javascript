3060f0a997cc0a069cc7f8e0d3f7a423db9213b9
root <ebrahimlaila815@gmail.com>
2024-09-18 22:43:36 +0000

    lint errors fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-/** The readable event handler manually reads chunks of data using 
+/** The readable event handler manually reads chunks of data using
  * process.stdin.read() and writes them to the output
- *  using process.stdout.write(). */ 
+ *  using process.stdout.write(). */
 process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name !== null) {
-      process.stdout.write(`Your name is: ${name}`);
-    }
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-  
+
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,35 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-    if (!fs.existsSync(path)) {
-        throw new Error('Cannot load the database');
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path, 'utf-8');
+  const lines = data.trim().split('\r\n').slice(1);
+  let numStds = 0;
+  const fields = {};
+  if (lines.length === 0) {
+    throw new Error('Cannot load the database');
+  }
+  for (const line of lines) {
+    const std = line.split(',');
+    if (fields[std[3]]) {
+      fields[std[3]].push(std[0]);
+    } else {
+      fields[std[3]] = [std[0]];
     }
-    if (!fs.statSync(path).isFile()) {
-        throw new Error('Cannot load the database');
+    numStds += 1;
+  }
+  console.log(`Number of students: ${numStds}`);
+  for (const field in fields) {
+    if (field) {
+      const list = fields[field];
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
     }
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.trim().split('\r\n').slice(1);
-    let numStds = 0;
-    let fields = {};
-    if (lines.length === 0) {
-        throw new Error('Cannot load the database');
-    }
-    for (let line of lines) {
-        const std = line.split(',');
-        if (fields[std[3]]) {
-            fields[std[3]].push(std[0]);
-        } else {
-            fields[std[3]] = [std[0]];
-        }
-        numStds += 1;
-    }
-    console.log(`Number of students: ${numStds}`);
-    for (let field in fields) {
-        if (field) {
-            const list = fields[field];
-            console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-        }
-    }
+  }
 };
 
 module.exports = countStudents;
