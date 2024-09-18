ed6bb83b50f9043fea27763fc54fad59427460cb
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-18 23:40:25 +0300

    Task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const helloStr = 'Welcome to Holberton School, what is your name?\n'
+
+rl.question(helloStr, (name) => {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log('This important software is now closing');
+    process.exit();
+});
