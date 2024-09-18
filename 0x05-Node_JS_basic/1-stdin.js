72c4056a9d9a5f35d7aa2f9e4dd4c5e910b7b41a
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-19 00:23:14 +0300

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 /** The readable event handler manually reads chunks of data using 
  * process.stdin.read() and writes them to the output
@@ -6,10 +6,10 @@ process.stdout('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
     const name = process.stdin.read();
     if (name !== null) {
-      process.stdout(`Your name is: ${name}`);
+      process.stdout.write(`Your name is: ${name}`);
     }
 });
   
 process.stdin.on('end', () => {
-    console.log('This important software is now closing\n');
+    process.stdout.write('This important software is now closing\n');
 });
