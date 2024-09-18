88dab89f12fb3a84b45cdd24a20c7541dd1c10a4
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-19 02:09:11 +0300

    Task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const app = http.createServer();
+const Port = 1245;
+const Host = 'localhost';
+
+app.on('request', (_, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+});
+
+app.listen(Port, Host);
+
+module.exports = app;
