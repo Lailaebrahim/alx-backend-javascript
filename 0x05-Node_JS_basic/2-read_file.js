2515e0b1566559ea02cb13dd4acd9193dc1cbe63
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-19 00:48:21 +0300

    Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,8 +2,25 @@ function countStudents(path) {
     try {
         const fs = require('fs');
         const data = fs.readFileSync(path, 'utf-8');
-        data = data.split('\n');
-        console.log(data)
+        const lines = data.trim().split('\r\n').slice(1);
+        let numStds = 0;
+        let fields = {};
+        for (let line of lines) {
+            const std = line.split(',');
+            if (fields[std[3]]) {
+                fields[std[3]].push(std[0]);
+            } else {
+                fields[std[3]] = [std[0]];
+            }
+            numStds += 1;
+        }
+        console.log(`Number of students: ${numStds}`);
+        for (let field in fields) {
+            if (field) {
+                const list = fields[field];
+                console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+            }
+        }
     }
     catch (error) {
         throw new Error('Cannot load the database');
