5d7b6407b43f86dc658246a555385cb4006b8c82
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-21 01:40:43 +0300

    Task 3: fix spy to be on the utils mpdule

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,34 +4,34 @@ const expect = require('chai').expect;
 const Utils = require('./utils.js').Utils;
 
 describe("sendPaymentRequestToApi", function () {
-    let calculateNumberSpy;
+    let UtilsSpy;
 
     // create a spy on property calculateNumber of Utils module
     beforeEach(function () {
-        calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+        UtilsSpy = sinon.spy(Utils);
     });
 
     // restore the spy after each test
     afterEach(function () {
-        calculateNumberSpy.restore();
+        UtilsSpy.calculateNumber.restore();
     });
 
     // check number of calling the internal method on calling the external one
     it("check if the function was called once", function() {
         sendPaymentRequestToApi(100, 20);
-        expect(calculateNumberSpy.callCount).to.be.equal(1);
+        expect(UtilsSpy.calculateNumber.callCount).to.be.equal(1);
     });
 
     // check the arguments the internal method is called with
     it("check called with arguments", function () {
         sendPaymentRequestToApi(100, 20);
-        expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+        expect(UtilsSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
     });
 
     // check the return value of the internal function
     it("check return value", function () {
         sendPaymentRequestToApi(100, 20);
-        expect(calculateNumberSpy.returned(120)).to.be.true;
+        expect(UtilsSpy.calculateNumber.returned(120)).to.be.true;
     });
 
 });
