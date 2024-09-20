25894eb706a7844adc44814e14adef7031488262
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-21 01:30:56 +0300

    Task 3: using sinon spy to test the call of calculateNumber method inside the sendPaymentRequestToApi

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -16,13 +16,22 @@ describe("sendPaymentRequestToApi", function () {
         calculateNumberSpy.restore();
     });
 
+    // check number of calling the internal method on calling the external one
     it("check if the function was called once", function() {
         sendPaymentRequestToApi(100, 20);
         expect(calculateNumberSpy.calledOnce).to.be.true;
     });
+
+    // check the arguments the internal method is called with
     it("check called with arguments", function () {
         sendPaymentRequestToApi(100, 20);
         expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
     });
-    
+
+    // check the return value of the internal function
+    it("check return value", function () {
+        sendPaymentRequestToApi(100, 20);
+        expect(calculateNumberSpy.returned(120)).to.be.true;
+    });
+
 });
