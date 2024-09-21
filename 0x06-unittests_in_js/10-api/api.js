b01fb2988da4424c32cd99089b41072d0b9b89be
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-21 05:25:32 +0300

    Fix

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const Port = 7865;
-app.use(express.json)
+app.use(express.json())
 
 app.get('/', (_, res) => {
     res.send('Welcome to the payment system');
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -5,7 +5,7 @@ describe('API integration test', function () {
 
     describe("Test suite for index route", () => {
         it('GET /', (done) => {
-            request.get('http://localhost:7865/', (err, res, body) => {
+            request.get('http://localhost:7865/', (_err, res, body) => {
                 expect(res.statusCode).to.be.equal(200);
                 expect(body).to.be.equal('Welcome to the payment system');
                 done();
@@ -15,7 +15,7 @@ describe('API integration test', function () {
 
     describe("Test suite for cart/: route", () => {
         it('Get /cart/:id', (done) => {
-            request.get('http://localhost:7865/cart/12', (err, res, body) => {
+            request.get('http://localhost:7865/cart/12', (_err, res, body) => {
                 expect(res.statusCode).to.be.equal(200);
                 expect(body).to.be.equal('Payment methods for cart 12');
                 done();
@@ -23,7 +23,7 @@ describe('API integration test', function () {
         });
 
         it('Get /cart/:id incorrect id', (done) => {
-            request.get('http://localhost:7865/cart/thisisnotanumber', (err, res, body) => {
+            request.get('http://localhost:7865/cart/thisisnotanumber', (_err, res, _body) => {
                 expect(res.statusCode).to.be.equal(404);
                 done();
             });
@@ -32,7 +32,7 @@ describe('API integration test', function () {
 
     describe("Test Suite for login route", () => {
         it('POST /login', (done) => {
-            request.post('http://localhost:7865/login', { json: { userName: 'Laila' } }, (err, res, body) => {
+            request.post('http://localhost:7865/login', { json: { userName: 'Laila' } }, (_err, res, body) => {
                 expect(res.statusCode).to.be.equal(200);
                 expect(body).to.be.equal('Welcome Laila');
                 done();
@@ -42,7 +42,7 @@ describe('API integration test', function () {
 
     describe("Test suite for available_payments route", () => {
         it('GET /available_payments', (done) => {
-            request.get('http://localhost:7865/available_payments', (err, res, body) => {
+            request.get('http://localhost:7865/available_payments', (_err, res, body) => {
                 expect(res.statusCode).to.be.equal(200);
                 expect(body).to.be.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
                 done();
