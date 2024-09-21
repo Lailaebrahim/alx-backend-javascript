85acc15fc835d3b69d36c3d7b4dddae3fb35d366
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-21 04:43:57 +0300

    Task 9 fix

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,7 +4,7 @@ const request = require('request')
 describe('API integration test', function () {
 
     it('GET /', (done) => {
-        request.get('http://localhost:7865/', (err, res, body) => {
+        request.get('http://localhost:7865/', (_err, res, body) => {
             expect(res.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Welcome to the payment system');
             done();
@@ -12,7 +12,7 @@ describe('API integration test', function () {
     });
 
     it('Get /cart/:id', (done) => {
-        request.get('http://localhost:7865/cart/12', (err, res, body) => {
+        request.get('http://localhost:7865/cart/12', (_err, res, body) => {
             expect(res.statusCode).to.be.equal(200);
             expect(body).to.be.equal('Payment methods for cart :12');
             done();
@@ -20,14 +20,14 @@ describe('API integration test', function () {
     });
 
     it('Get /cart/:id incorrect id', (done) => {
-        request.get('http://localhost:7865/cart/thisisnotanumber', (err, res, body) => {
+        request.get('http://localhost:7865/cart/thisisnotanumber', (_err, res, _body) => {
             expect(res.statusCode).to.be.equal(404);
             done();
         });
     });
 
     it('Get /cart/:id negative id', (done) => {
-        request.get('http://localhost:7865/cart/-108', (err, res, body) => {
+        request.get('http://localhost:7865/cart/-108', (_err, res, _body) => {
             expect(res.statusCode).to.be.equal(404);
             done();
         });
