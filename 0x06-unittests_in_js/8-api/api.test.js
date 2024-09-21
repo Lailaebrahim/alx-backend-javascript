4df86e2551f3c814d3e27bbb3fe7adc74775af4c
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-21 03:56:56 +0300

    Task 8: creating a node server using express and testing it async

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,6 +1,4 @@
-const app = require('./api');
-const chai = require('chai')
-const expect = chai.expect
+const expect = require('chai').expect
 const request = require('request')
 
 describe('API integration test', function () {
